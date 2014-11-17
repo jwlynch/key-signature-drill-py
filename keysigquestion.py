4d3f3a5a6acbfd0a1950d2307ea885082803adc9
Jim Lynch <jim@jam.sessionsnet.org>
2014-11-17 00:51:54 -0800

    Add question, supply key given key signature

--- keysigquestion.py
+++ keysigquestion.py
@@ -1,3 +1,4 @@
+import random
 from key import Key
 
 class KeySigQuestion(object):
@@ -5,11 +6,21 @@ class KeySigQuestion(object):
         self.itsKey = Key()
 
     def ask(self):
-        return self.givenKeyGiveAccidentals()
+        choice = []
+        choice.append(self.givenKeyGiveAccidentals)
+        choice.append(self.givenSignatureGiveKey)
+        return random.choice(choice)()
 
     def givenKeyGiveAccidentals(self):
         q = "What's the key signature of " + self.itsKey.key[0] + "? "
         a = raw_input(q)
         result = (a == self.itsKey._text_answer())
+        
+        return result
+    
+    def givenSignatureGiveKey(self):
+        q = "What key has " + self.itsKey._text_answer() + "? "
+        a = raw_input(q)
+        result = (a == self.itsKey.key[0])
         
         return result
