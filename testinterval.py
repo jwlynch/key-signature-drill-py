ddecd99f648eac9188187bd7b324326ed4558e8f
Jim Lynch <jim@jam.sessionsnet.org>
2015-02-04 11:27:42 -0800

    add testBasicIntervalTable() complete

--- testinterval.py
+++ testinterval.py
@@ -34,5 +34,47 @@ class TestInterval(unittest.TestCase):
         self.assertEqual(str(p_5), "perfect fifth or twelvth")
         self.assertEqual(str(d_5), "diminished fifth or twelvth")
 
+    def testBasicIntervalTable(self):
+        mi_2 = Interval(2, "mi", False)
+        ma_2 = Interval(2, "Ma", False)
+        mi_3 = Interval(3, "mi", False)
+        ma_3 = Interval(3, "Ma", False)
+        mi_6 = Interval(6, "mi", False)
+        ma_6 = Interval(6, "Ma", False)
+        mi_7 = Interval(7, "mi", False)
+        ma_7 = Interval(7, "Ma", False)
+
+        p_1 = Interval(1, "p", True)
+        p_4 = Interval(4, "p", True)
+        a_4 = Interval(4, "a", True)
+        p_5 = Interval(5, "p", True)
+        d_5 = Interval(5, "d", True)
+
+        letter_list = ["C", "D", "E", "F", "G", "A", "B"]
+
+        table_list = [Interval(x, y) for x in letter_list for y in letter_list]
+
+        interval_list = \
+        [
+            p_1, ma_2, ma_3, p_4, p_5, ma_6, ma_7,
+            mi_7, p_1, ma_2, mi_3, p_4, p_5, ma_6,
+            mi_6, mi_7, p_1, mi_2, mi_3, p_4, p_5,
+            p_5, ma_6, ma_7, p_1, ma_2, ma_3, a_4,
+            p_4, p_5, ma_6, mi_7, p_1, ma_2, ma_3,
+            mi_3, p_4, p_5, mi_6, mi_7, p_1, ma_2,
+            mi_2, mi_3, p_4, d_5, mi_6, mi_7, p_1
+        ]
+
+        for dex in range(len(interval_list)):
+            #print
+            #(
+            #    str(dex) +
+            #    " " +
+            #    str(interval_list[dex]) +
+            #    " " +
+            #    str(table_list[dex])
+            #)
+            self.assertEqual(interval_list[dex], table_list[dex])
+
 if __name__ == '__main__':
     unittest.main()
