eb98a6a918bd2aa57be020dd2174982ffbfc57cf
Jim Lynch <jim@jam.sessionsnet.org>
2015-02-08 10:04:40 -0800

    new file intervalquestion.py initially copy of keysigquestion.py

---
+++ intervalquestion.py
@@ -0,0 +1,46 @@
+import random
+from key import Key
+
+PY2 = (str is bytes)
+
+if PY2:
+    my_input = raw_input
+else:
+    my_input = input
+
+class KeySigQuestion(object):
+    def __init__(self, aKey=None):
+        if aKey is None:
+            self.itsKey = Key()
+        else:
+            self.itsKey = Key(aKey)
+
+        self.majorKeyName = self.itsKey.key[0]
+
+    def ask(self):
+        choice = []
+        choice.append(self.givenKeyGiveAccidentals)
+        choice.append(self.givenSignatureGiveKey)
+        choice.append(self.givenAccidentalsGiveKey)
+        return random.choice(choice)()
+
+    def givenKeyGiveAccidentals(self):
+        q = "What's the key signature of " + self.majorKeyName + "? "
+        a = my_input(q).strip()
+        result = (a == self.itsKey.textual_key_signature())
+        
+        return result
+    
+    def givenSignatureGiveKey(self):
+        q = "What key has " + self.itsKey.textual_key_signature() + "? "
+        a = my_input(q).strip()
+        result = (a == self.majorKeyName)
+        
+        return result
+
+    def givenAccidentalsGiveKey(self):
+        q = "What key has " + ", ".join(self.itsKey.accidentals()) + "? "
+        a = my_input(q).strip()
+        result = (a == self.majorKeyName)
+        
+        return result
