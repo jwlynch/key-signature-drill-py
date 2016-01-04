9f87046462fe0e89bf6c65d6432d68eb330e5054
Jim Lynch <jim@jam.sessionsnet.org>
2016-01-04 00:01:40 -0800

    func majScales() returns the list of major scales in key order

---
+++ majScales.py
@@ -0,0 +1,26 @@
+# key list, for diatonic interval questions
+
+def majScales():
+    maj_scale_list = \
+        [ \
+            ["Cb", "Db", "Eb", "Fb", "Gb", "Ab", "Bb"],
+            ["Gb", "Ab", "Bb", "Cb", "Db", "Eb", "F"],
+            ["Db", "Eb", "F", "Gb", "Ab", "Bb", "C"],
+            ["Ab", "Bb", "C", "Db", "Eb", "F", "G"],
+            ["Eb", "F", "G", "Ab", "Bb", "C", "D"],
+            ["Bb", "C", "D", "Eb", "F", "G", "A"],
+            ["F", "G", "A", "Bb", "C", "D", "E"],
+            ["C", "D", "E", "F", "G", "A", "B"],
+            ["G", "A", "B", "C", "D", "E", "F#"],
+            ["D", "E", "F#", "G", "A", "B", "C#"],
+            ["A", "B", "C#", "D", "E", "F#", "G#"],
+            ["E", "F#", "G#", "A", "B", "C#", "D#"],
+            ["B", "C#", "D#", "E", "F#", "G#", "A#"],
+            ["F#", "G#", "A#", "G", "C#", "D#", "E#"],
+            ["C#", "D#", "E#", "F#", "G#", "A#", "B#"]
+        ]
+
+    return maj_scale_list
+
+
+
